ce7c9d2cabc8d0df5d408752fdfdc11e5bff7ee5
Snehaaaluvscheese <snehakrishnani255@gmail.com>
2023-03-04 09:41:04 +0530

    Add hover effect to grid and add button that asks user for squares each side

--- script.js
+++ script.js
@@ -1,11 +1,53 @@
-const container = document.querySelector('#container');
+const gridContainer = document.querySelector('#grid-container');
+
+const div1 = document.createElement('div');
+div1.classList.add('div1');
+gridContainer.appendChild(div1);
 
 
 
-for(let i = 1; i < 257; i++) {
-const div = document.createElement('div');
-div.style.cssText = "border: 1px solid black; height: 25px; width: 25px"; 
-container.appendChild(div);
+for (let i = 1; i < 256; i++){
+    const div2 = document.createElement('div');
+    div2.classList.add('div2');
+    gridContainer.appendChild(div2);
 
+    div2.addEventListener('mouseover', () => {
+        div2.classList.add('drawLines');
+    });
+    
 }
+
+div1.addEventListener('mouseover', () => {
+    div1.classList.add('drawLines');
+});
+
+function gridSquares() {
+   let result = prompt("Number of squares (not more than 100) per side?");
+   if (result < 101) {
+    
+        gridContainer.setAttribute('style', `
+            display: grid;
+            width: 400px;
+            height: 400px;
+            grid-column-start: 1;
+            grid-column-end:${result}; 
+            grid-row-start: 1;
+            grid-row-end: ${result};
+            margin-left: 20cm;
+            margin-top: 8cm;
+            padding: 0;
+          
+          `);
+
+        gridContainer.removeAttribute('id', 'grid-container');
+    
+
+   }
+}
+
+const btn = document.querySelector('#gridButton');
+btn.addEventListener('click', () => { 
+    gridSquares();
+});
+
 
